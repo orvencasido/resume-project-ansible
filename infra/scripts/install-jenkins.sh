a7d432b77051aebde6d80ec3f7d6ec8e85e05262
Ubuntu <ubuntu@ip-172-31-29-214.ap-southeast-1.compute.internal>
2025-09-05 07:18:10 +0000

    update

--- infra/scripts/install-jenkins.sh
+++ infra/scripts/install-jenkins.sh
@@ -1,14 +1,24 @@
 #!/bin/bash 
 set -e
 
+until k3s kubectl get nodes &>/dev/null; do
+    echo "Kubernetes not ready, sleeping 5s..."
+    sleep 5
+done
+
+mkdir -p ~/.kube
+sudo k3s kubectl config view --raw > ~/.kube/config
+chmod 600 ~/.kube/config
+export KUBECONFIG=~/.kube/config
+
+kubectl create namespace jenkins --dry-run=client -o yaml | kubectl apply -f -
+
 helm repo add jenkins https://charts.jenkins.io
 helm repo update
-
-kubectl create namespace jenkins 
 
 helm install jenkins jenkins/jenkins -n jenkins \
 --set persistence.enabled=true \
 --set persistence.size=8Gi \
 --set persistence.storageClass=local-path \
---set service.type=NodePort \
---set service.nodePort=30080
+--set controller.service.type=NodePort \
+--set controller.service.nodePort=30080
--- infra/scripts/install-k3s.sh
+++ infra/scripts/install-k3s.sh
@@ -3,7 +3,3 @@ set -e
 
 curl -sfL https://get.k3s.io | sh -
 
-export KUBECONFIG=~/.kube/config && \
-mkdir ~/.kube 2> /dev/null && \
-k3s kubectl config view --raw > "$KUBECONFIG" && \
-chmod 600 "$KUBECONFIG" 
